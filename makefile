842bae262a98c6fa1f769b9430edb0801c225c4c
seattlenomnom <mark.crapser@gmail.com>
2023-06-28 13:03:28 -0700

    first commit

---
+++ makefile
@@ -0,0 +1,4 @@
+# makefile for ptr11c13
+#
+ptr11c13: ptr11c13.c
+	gcc -g ptr11c13.c -o Build\ptr11c13
---
+++ ptr11c13.c
@@ -0,0 +1,73 @@
+/* source file for ptr11c13.c
+ *
+ * ptr11c13 is a demo program to show the use of pointers to arrays
+ * in implementing a copy function.
+*/
+
+/* includes */
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* defines */
+
+
+
+
+
+
+
+/* function declarations */
+
+
+
+
+
+
+
+
+
+
+
+int maint(int argc, char argv[]){
+
+
+
+
+
+
+
+
+
+    return(0);
+}
+
+/* function definitions */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
